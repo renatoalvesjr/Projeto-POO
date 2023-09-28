34e47124edf6965529e1b7479ebe27a7ab391810
Renato <grangamin@gmail.com>
2023-09-28 13:55:11 -0300

    Update Avaliacao.java

--- src/itens/Avaliacao.java
+++ src/itens/Avaliacao.java
@@ -21,23 +21,25 @@ public class Avaliacao {
     
     public Avaliacao() {
         this.id = ++Avaliacao.serial;
+        calcIMC();
+        calcTMB();
+        calcBF();
+        this.rotina = pessoa.getUserType();
         this.createDate = Datas.dataAgora();
         this.modifyDate = "";
     }
     
-    public void calcIMC(){
+    private void calcIMC(){
         this.imc = this.peso/Math.pow(this.altura,2);
-        this.modifyDate += Datas.dataAgora();
     }
-    public void calcTMB(){
+    private void calcTMB(){
         if(this.pessoa.getSexo().equalsIgnoreCase("m")){
             this.tmb = this.rotina*(66+((13.7*this.peso)+(5*this.altura)-(6.8*this.idade)));
         }else{
             this.tmb = this.rotina*(655+((9.6*this.peso)+(1.8*this.altura)-(4.7*this.idade)));
         }
-        this.modifyDate += Datas.dataAgora();
     }
-    public void calcBF(){
+    private void calcBF(){
         if(this.pessoa.getSexo().equalsIgnoreCase("m")){
             this.bf = (163.205*Math.log10(this.cintura+this.quadril-this.pescoco))-(97.684*Math.log10(this.altura))-78.387;
         }else{
@@ -45,7 +47,6 @@ public class Avaliacao {
         }
         this.massMagra = this.peso*(1-(this.bf/100));
         this.massGorda = this.peso*(this.bf/100);
-        this.modifyDate += Datas.dataAgora();
     }
 
     public long getId() {
